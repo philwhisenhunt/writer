83b13048e491380e2abee20971e95a38738d9e90
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-11-11 16:15:17 -0500

    Clear up routes to use resources instead of single use get commands

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'blog_posts/new'
-  get 'blog_posts/edit'
-  get 'blog_posts/delete'
+  resources :blog_posts
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 end
