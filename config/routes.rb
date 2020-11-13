ebcb75bb0b0630ac03bc0cc457c095550e76d9ae
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-11-12 21:31:48 -0500

    route to correct home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :blog_posts
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  root to: "pages#main"
 end
