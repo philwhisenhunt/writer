7f2874a5691de9b499c908cd7498d6535754cfb1
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-11-13 22:36:29 -0500

    Generate controller for users

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,10 @@
+class UserController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/new'
+  get 'user/edit'
+  get 'user/destroy'
   resources :blog_posts
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
