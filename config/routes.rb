68fdcb1f7a77585a0670f88ff74254b45408dc7b
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-11-25 14:40:05 -0500

    Add users resource to view and create users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'user/edit'
   get 'user/destroy'
   resources :blog_posts
-  get '/login' to: 'pages#login'
+  resources :users
+  get '/login', to: 'pages#login'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#main"
