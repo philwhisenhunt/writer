7c112d8a318fe99e3dcdd0c0531deffcc736ab68
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-13 20:30:42 -0500

    Updated schema by rerunning stuff
    db:migrate:reset did the trick, things working as on other machine

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_230006) do
+ActiveRecord::Schema.define(version: 2020_11_14_155521) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "post_content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
