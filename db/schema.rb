4b35a94b98428a60e7f61462c543b45c106ea496
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-23 22:23:20 -0500

    New schema from new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_155521) do
+ActiveRecord::Schema.define(version: 2020_12_23_203843) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "post_content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_blog_posts_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -24,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_11_14_155521) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "blog_posts", "users"
 end
